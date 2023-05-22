f4913aa5d60be965bc11b896dbf268debb5d9af3
saikatsaha36 <saikat.k.k.aiub@gmail.com>
2023-05-23 03:41:29 +0600

    home slider and category screen api done

--- lib/ui/screens/bottom_nav_bar_screen.dart
+++ lib/ui/screens/bottom_nav_bar_screen.dart
@@ -4,8 +4,11 @@ import 'package:ostad_flutter_batch_two/ui/screens/category_screen.dart';
 import 'package:ostad_flutter_batch_two/ui/screens/home_screen.dart';
 import 'package:ostad_flutter_batch_two/ui/screens/wish_list_screen.dart';
 import 'package:ostad_flutter_batch_two/ui/state_managers/bottom_navigation_bar_controller.dart';
+import 'package:ostad_flutter_batch_two/ui/state_managers/home_controller.dart';
 import 'package:ostad_flutter_batch_two/ui/utils/app_colors.dart';
 import 'package:get/get.dart';
+
+import '../state_managers/category_controller.dart';
 
 class BottomNavBarScreen extends StatefulWidget {
   const BottomNavBarScreen({Key? key}) : super(key: key);
@@ -23,50 +26,44 @@ class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    Get.find<HomeController>().getHomeSlider();
+    Get.find<CategoryController>().getCategories();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GetBuilder<BottomNavigationBarController>(
-
-        builder: (controller) {
-          return _screens[controller.selectedIndex];
-        }
-      ),
-      bottomNavigationBar: GetBuilder<BottomNavigationBarController>(
-
-        builder: (controller) {
-          return BottomNavigationBar(
-            onTap: (value) {
-              controller.changeIndex(value);
-
-            },
-            elevation: 5,
-            selectedItemColor: primaryColor,
-            unselectedItemColor: softGreyColor,
-            currentIndex: controller.selectedIndex,
-            showUnselectedLabels: true,
-            items: const [
-              BottomNavigationBarItem(
+      body: GetBuilder<BottomNavigationBarController>(builder: (controller) {
+        return _screens[controller.selectedIndex];
+      }),
+      bottomNavigationBar:
+          GetBuilder<BottomNavigationBarController>(builder: (controller) {
+        return BottomNavigationBar(
+          onTap: (value) {
+            controller.changeIndex(value);
+          },
+          elevation: 5,
+          selectedItemColor: primaryColor,
+          unselectedItemColor: softGreyColor,
+          currentIndex: controller.selectedIndex,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
                 icon: Icon(
                   Icons.home_outlined,
                 ),
-                label: 'Home'
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.grid_view),
-                label: 'Categories'
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart_outlined),
-                label: 'Cart'
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_border),
-                label: 'Wishlist'
-              ),
-            ],
-          );
-        }
-      ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.grid_view), label: 'Categories'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart_outlined), label: 'Cart'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border), label: 'Wishlist'),
+          ],
+        );
+      }),
     );
   }
 }
